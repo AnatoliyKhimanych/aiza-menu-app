655f828b4c9d6c47ded251dfcba07752c2020aa3
Anatoly <code4webdev@gmail.com>
2025-08-04 13:37:33 +0500

    chore: удалил App.css, импортировал global.css в index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/style/global.css'; //? Импорт глобальных стилей
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
